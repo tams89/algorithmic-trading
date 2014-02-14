351165a1b374099f6f97b2ddaa7436675b311977
tam.s89 <tam.s89@gmail.com>
2014-02-14 14:15:21 +0000

    - Added Interactive Brokers API - Added Test outline for IB API. (Needs refactoring)

---
+++ Test/IBImpl/ScannerSubscriptionSamples.cs
@@ -0,0 +1,19 @@
+﻿/* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+using IBApi;
+
+namespace Test.UnitTest.IBImpl
+{
+    public class ScannerSubscriptionSamples
+    {
+        public static ScannerSubscription GetScannerSubscription()
+        {
+            var scanSub = new ScannerSubscription();
+            scanSub.Instrument = "STOCK.EU";
+            scanSub.LocationCode = "STK.EU.IBIS";
+            scanSub.ScanCode = "HOT_BY_VOLUME";
+            return scanSub;
+        }
+    }
+}
