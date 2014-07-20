f65f119505c5f4516fe867ed876482fb5dbec00b
Tamesh Sivaguru <tam.s89@gmail.com>
2014-07-20 17:39:22 +0100

    +

--- SQL Scripts/Iteration Analysis Script.sql
+++ SQL Scripts/Iteration Analysis Script.sql
@@ -2,12 +2,12 @@ USE [SivaguruCapital]
 
 -- All iterations
 SELECT * 
-FROM Portfolio.Iteration
+FROM [dbo].Iteration
 ORDER BY CurrentCash DESC
 
 -- Number of orders by iteration
 SELECT TOP 1 COUNT(IterationId) AS 'Total Orders', IterationId, Symbol
-FROM Portfolio.[Order]
+FROM [dbo].[Order]
 GROUP BY IterationId, Symbol
 ORDER BY 
 COUNT(IterationId) DESC
@@ -16,19 +16,19 @@ COUNT(IterationId) DESC
 -- Watch to see if number of trades begins to approach a significant
 -- portion of the volume for that day.
 SELECT Date, COUNT(Date) AS 'Orders'
-FROM Portfolio.[Order] A
+FROM [dbo].[Order] A
  INNER JOIN
  (SELECT TOP 1 IterationId
-  FROM Portfolio.[Order]
+  FROM [dbo].[Order]
   GROUP BY IterationId, Symbol
   ORDER BY COUNT(IterationId) DESC) B 
   ON A.IterationId = B.IterationId
 GROUP BY Date
 ORDER BY COUNT(DATE) DESC
 
-SELECT O.Date, O.Quantity, O.Value
-FROM SivaguruCapital.Portfolio.[Order] O
-INNER JOIN SivaguruCapital.Portfolio.Iteration I
-ON O.IterationId = I.IterationId
-WHERE I.IterationId = '36EC880C-0C15-4F2C-8904-56E3EC434D88'
-ORDER BY Date
+--SELECT O.Date, O.Quantity, O.Value
+--FROM SivaguruCapital.[dbo].[Order] O
+--INNER JOIN SivaguruCapital.[dbo].Iteration I
+--ON O.IterationId = I.IterationId
+--WHERE I.IterationId = '36EC880C-0C15-4F2C-8904-56E3EC434D88'
+--ORDER BY Date
