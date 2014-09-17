581f284b86c91ccacb60617438d6c4f42d6aad1d
Tamesh Sivaguru <tam.s89@gmail.com>
2014-09-17 15:33:50 +0100

    - main window renamed and resized

---
+++ Algorithm.GUI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Windows;
+using Algorithm.Core.YahooFinanceService;
+
+namespace Algorithm.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Query.QueryService _queryService = new Query.QueryService("APPL");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var data = _queryService.GetStockPrice.ToList();
+        }
+    }
+}
